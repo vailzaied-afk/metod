c936847fea0f662247b0d681b6a64161cbfd4a3a
Женя <vailzaied@gmail.com>
2026-04-14 21:59:14 +0300

    +

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@ import java.time.LocalDate;
 
 public class Main {
 
-    public static int determineTheYear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + " год високосный");
-            return 1;
-        }
-        System.out.println(year + " год не является високосным");
-        return 0;
+    public static boolean determineTheYear(int year) {
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
     }
 
     public static void checkDeviceAndOS(int oS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
         String platform = "";
 
         if (oS == 0) {
@@ -24,7 +22,7 @@ public class Main {
         }
 
 
-        String version = (clientDeviceYear < 2015) ? "облегченную версию" : "версию";
+        String version = (clientDeviceYear < currentYear) ? "облегченную версию" : "версию";
 
 
         System.out.println("Установите " + version + " приложения для " + platform + " по ссылке");
@@ -34,11 +32,11 @@ public class Main {
         int deliveryDays = 1;
 
         if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + deliveryDays);
+            System.out.println(deliveryDays);
         } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + (deliveryDays + 1));
+            System.out.println(deliveryDays + 1);
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: " + (deliveryDays + 2));
+            System.out.println(deliveryDays + 2);
         } else {
             System.out.println("На такое расстояние не возим");
         }
@@ -48,13 +46,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int year = 2024;
+        int year = 2000;
 
         if (year < 1584) {
             System.out.println("Григорианский календарь еще не был введен");
         } else {
-            determineTheYear(year);
+
+            if (determineTheYear(year)) {
+                System.out.println(year + " — високосный год");
+            } else {
+                System.out.println(year + " — не високосный год");
+            }
         }
+
         System.out.println("Задание 2");
 
         checkDeviceAndOS(0, 2026);
